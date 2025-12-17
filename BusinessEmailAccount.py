7ad170c3ec0fd1eeea3503d18b3f6a5d12a56b07
creyr <creyes.geo@gmail.com>
2025-12-16 19:21:28 -0600

    Add first Python script
    Initial Python exercise focused on input/output and string handling.

---
+++ BusinessEmailAccount.py
@@ -0,0 +1,19 @@
+##### This company needs to generate business email accounts for their workers
+
+print("In order to give you your new business email account I will need the following information:")
+
+#Variables needed
+FamilyName = input("      *Family Name:").upper()
+Name = input("        *Name:").upper()
+Year = (input("     *You were born in (year):"))
+Month = (input("      *Month(número 1-12):"))
+
+letter_1 = FamilyName [0]
+letter_2 = FamilyName [1]
+letter_3 = FamilyName [2]
+letter_4 = Name[0]
+letter_5 = Name[1]
+email = letter_1 + letter_2 + letter_3 + letter_4 + letter_5 + Year + Month + "@bussiness.account.com"
+
+print("Hello!", Name, FamilyName)
+print("Your new business account is", email)
